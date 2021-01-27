0e0939aad42d713bc30b911fd2f05ed394863625
samgates3 <69642365+samgates3@users.noreply.github.com>
2021-01-27 13:56:00 -0600

    new try

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from 'react';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
+
+const roomStyle = {
+  width: 400,
+  height: 400
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Content
       </header>
-      <div id="otEmbedContainer" style="width:400px; height:400px">
-      <script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=1f1059f3-ed6d-4a1b-9fd0-01f988a0bdea&room=DEFAULT_ROOM"></script>
+      <div id="otEmbedContainer" style={roomStyle}>
       </div>
+      <script id="otEmbedContainer" src="https://tokbox.com/embed/embed/ot-embed.js?embedId=1f1059f3-ed6d-4a1b-9fd0-01f988a0bdea&room=DEFAULT_ROOM"></script>
       <AmplifySignOut />
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default withAuthenticator(App, true);
