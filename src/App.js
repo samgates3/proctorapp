8cf586de789080a9f715b291d805e327d640048f
samgates3 <69642365+samgates3@users.noreply.github.com>
2021-01-27 13:44:52 -0600

    added room

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <header className="App-header">
         Content
       </header>
+      <div id="otEmbedContainer" style="width:400px; height:400px">
+      <script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=1f1059f3-ed6d-4a1b-9fd0-01f988a0bdea&room=DEFAULT_ROOM"></script>
+      </div>
       <AmplifySignOut />
     </div>
   );
