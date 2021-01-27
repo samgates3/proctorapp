b583d7c097e417694f1a790d6409f59b225ca99c
samgates3 <69642365+samgates3@users.noreply.github.com>
2021-01-27 14:12:19 -0600

    next try

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 
-const roomStyle = {
-  width: 400,
-  height: 400
-}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        Content
+        <AmplifySignOut />
       </header>
-      <div id="otEmbedContainer" style={roomStyle}>
+      <div id="otEmbedContainer">
+        <script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=1f1059f3-ed6d-4a1b-9fd0-01f988a0bdea&room=DEFAULT_ROOM"></script>
       </div>
-      <script id="otEmbedContainer" src="https://tokbox.com/embed/embed/ot-embed.js?embedId=1f1059f3-ed6d-4a1b-9fd0-01f988a0bdea&room=DEFAULT_ROOM"></script>
-      <AmplifySignOut />
     </div>
   );
 }
