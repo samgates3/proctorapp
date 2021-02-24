abcdf9c7fd00f1796665d0c2db9e88149ea2c830
samgates3 <69642365+samgates3@users.noreply.github.com>
2021-02-24 14:12:44 -0500

    new try

--- src/Components/Webcams.js
+++ src/Components/Webcams.js
@@ -6,16 +6,16 @@ function Webcams(){
     const room1=useRef(null);
 
     useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://tokbox.com/embed/embed/ot-embed.js?embedId=1f1059f3-ed6d-4a1b-9fd0-01f988a0bdea&room=DEFAULT_ROOM';
-
-        room1.current.appendChild(script);
+        const scripty = document.createElement('script');
+        scripty.src = 'https://tokbox.com/embed/embed/ot-embed.js?embedId=1f1059f3-ed6d-4a1b-9fd0-01f988a0bdea&room=DEFAULT_ROOM';
+        document.getElementById('room1').appendChild(scripty)
+        
     }, []);
     return (
         <div>
             <Navbar />
             <h1>Webcams Page</h1>
-            <div ref={room1} width="400" height="400"></div>
+            <div id='room1' ref={room1} width="400" height="400" />
         </div>
     )
 }
